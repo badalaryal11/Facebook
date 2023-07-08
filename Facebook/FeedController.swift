46ff9263528ac60aa1524bdc64e81b1a83d644c4
Badal Aryal <badal.aryal@gmail.com>
2023-07-09 00:17:46 +0545

    Title bar of facebook dashboard is made.

---
+++ Facebook/FeedController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Facebook
+//
+//  Created by Badal  Aryal on 08/07/2023.
+//
+
+import UIKit
+
+class FeedController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Facebook Dashboard"
+        self.navigationController?.navigationBar.backgroundColor = .blue
+        navigationController?.navigationBar.titleTextAttributes = [
+                    NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 15),
+                    NSAttributedString.Key.foregroundColor: UIColor.white
+                ]
+        // Do any additional setup after loading the view.
+   
+    
+    }
+
+
+}
+

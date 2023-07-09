99289dc032b9edfa0cfc691ab51191d60d32b6dc
Badal Aryal <badal.aryal@gmail.com>
2023-07-09 20:26:45 +0545

    error in posts.count

--- Facebook/FeedController.swift
+++ Facebook/FeedController.swift
@@ -7,21 +7,45 @@
 
 import UIKit
 
-class FeedController: UIViewController {
-
+class FeedController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var table : UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        table.delegate = self
+        table.dataSource = self
         title = "Facebook Dashboard"
         self.navigationController?.navigationBar.backgroundColor = .blue
         navigationController?.navigationBar.titleTextAttributes = [
-                    NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 15),
-                    NSAttributedString.Key.foregroundColor: UIColor.white
-                ]
+            NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 15),
+            NSAttributedString.Key.foregroundColor: UIColor.white
+        ]
         // Do any additional setup after loading the view.
-   
+        
+        
     
     }
-
-
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+        
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return posts.count
+    }
+    
+    
+    struct FacebookPost{
+        let numberOfLikes:Int
+        let username:String
+        let userImagename:String
+        let postImagename:String
+    }
+    
+    
 }
-
--- Facebook/PostTableViewCell.swift
+++ Facebook/PostTableViewCell.swift
@@ -8,16 +8,23 @@
 import UIKit
 
 class PostTableViewCell: UITableViewCell {
+    @IBOutlet var userImageView: UIImageView!
+    @IBOutlet var postImageView: UIImageView!
+    @IBOutlet var likesLabel: UILabel!
+    @IBOutlet var commentsLabel: UILabel!
+    @IBOutlet var shareLabels: UILabel!
+    
 
     static let identifier = "PostTableViewCell"
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        func nib() -> UINib
+    
+        static func nib() -> UINib
         {
             return UINib (nibName: "PostTableViewCell",
                           bundle: nil)
         }
         // Initialization code
+    override func awakeFromNib() {
+        super.awakeFromNib()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -25,5 +32,12 @@ class PostTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
-
+    func configure(with model: FacebookPost)
+    {
+        self.likesLabel.text = "\(model.numberOfLikes)"
+        self.usernameLabel.text = model.username
+        self.userImageView.image = UIImage(named: model.userImagename)
+        self.userImageView.image = UIImage(named: model.userImagename)
+        self.postImageView.image = UIImage(named: model.postImagename)
+    }
 }

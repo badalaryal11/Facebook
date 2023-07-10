cb639bea605c8dbf90d8c4dacd4a78c4fc8f39f5
Badal Aryal <badal.aryal@gmail.com>
2023-07-11 00:52:27 +0545

    almost complete with only one error.

--- Facebook/Post.swift
+++ Facebook/Post.swift
@@ -15,23 +15,23 @@ struct Post {
     var noOfComments: Int?
     var noOfShares: Int?
     
-    }
+    
 
-func fetchPosts() -> [Post]
+   static func fetchPosts() -> [Post]
 {
-   var posts = [Post]()
+    var posts = [Post]()
     let zuck = User(username: "Mark Zuckerberg",profileImage: UIImage(named: "mark"))
     let post1 =  Post(createdBy: zuck, timeAgo: "5 hrs", Caption: " The essential struggles of humanity is the harnessing the power of technology for the betterment of humanity. The essential struggles of humanity is the harnessing the power of technology for the betterment of humanity.- [ ] The essential struggles of humanity is the harnessing the power of technology for the betterment of humanity. The essential struggles of humanity is the harnessing the power of technology for the betterment of humanity. The essential struggles of humanity is the harnessing the power of technology for the betterment of humanity. The essential struggles of humanity is the harnessing the power of technology for the betterment of humanity The essential struggles of humanity is the harnessing the power of technology for the betterment of humanity.", noOfLikes: 134, noOfComments: 145, noOfShares: 147)
     
     let badal = User(username: "Badal Aryal",profileImage: UIImage(named: "badal"))
     let post2 =  Post(createdBy: badal, timeAgo: "11 hrs", Caption: " The essential struggles of humanity is the harnessing the power of technology for the betterment of humanity. The essential struggles of humanity is the harnessing the power of technology for the betterment of humanity.- [ ] The essential struggles of humanity is the harnessing the power of technology for the betterment of humanity. The essential struggles of humanity is the harnessing the power of technology for the betterment of humanity. The essential struggles of humanity is the harnessing the power of technology for the betterment of humanity. The essential struggles of humanity is the harnessing the power of technology for the betterment of humanity The essential struggles of humanity is the harnessing the power of technology for the betterment of humanity.", noOfLikes: 11, noOfComments: 14, noOfShares: 23)
- 
+    
     posts.append(post1)
     posts.append(post2)
     
     return posts
     
-    
+}
 }
 
 struct User{
